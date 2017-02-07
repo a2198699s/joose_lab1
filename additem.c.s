04aa64886aee85acdacd269d707ee95388edb132
Andrew Smith <2198699s@student.gla.ac.uk>
2017-02-07 14:22:33 +0000

    updated c.s. file

--- additem.c.s
+++ additem.c.s
@@ -1,13 +1,13 @@
 # Barcode
-	Exists:
+	BcodeExists:
 		null.							[property noBarcode] [single]
 		Not null.						
-	Content:
+	BcodeContent:
 		Only contains numbers.          [if !noBarcode]
 		Contains other characters. 		[if !noBarcode] [property invalid]
 		Is a single number. 			[if !noBarcode] [single]
 	
-	Value:
+	BcodeValue:
 		<99.							[if !invalid && !noBarcode] 
 		99.								[if !invalid && !noBarcode] [single]
 		>99 and <9999.					[if !invalid && !noBarcode]
@@ -16,10 +16,10 @@
 										 
 	
 # itemName 
-	Exists:
+	NameExists:
 		null.                          	[property emptyName] [single]
 		Not null.
-	Content:
+	NameContent:
 		Empty.							[if !emptyName] [single]
 		No special characters.			[if !emptyName]
 		Contains special characters.	[if !emptyName] 
@@ -32,13 +32,13 @@
 			
 	
 # qty
-	Exists:
+	QuantityExists:
 		null.							[property noQty] [single]
 		Not null.
-	Content:
+	QuantityContent:
 		Only contains numbers.			[if !noQty]
 		Contains other characters. 		[if !noQty] [single]
-	Value:
+	QuantityValue:
 		<0.								[if !noQty]	[single]	 
 		0.								[if !noQty] [single]
 		>0 and <100.					[if !noQty]
@@ -46,20 +46,20 @@
 		>100.							[if !noQty]	[single]				 
 
 # supplier
-	Exists:
+	SupplierExists:
 		null.							[property noSupplier] [single]
 		Not null.
-	Content:
+	SupplierContent:
 		Empty.							[if !noSupplier] [single]
 		Contains special characters.	[if !noSupplier] [single]	 
 		No special characters.			[if !noSupplier]
 
 
 # desc
-	Exists:
+	DescExists:
 		null.                        	[property noDesc] [single]
 		Not null.
-	Content:
+	DescContent:
 		Empty.							[if !noDesc] [single]
 		Contains special characters.	[if !noDesc] [single]
 		No special characters.			[if !noDesc]
